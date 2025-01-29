50fe3261abed6abd7bfc24e8cdd5c5346294b409
Jaiden Khosla <jaiden@khosla.io>
2025-01-28 21:26:54 -0600

    dialogSystem

--- scenes/scripts/dialog_system.gd
+++ scenes/scripts/dialog_system.gd
@@ -1,13 +1,29 @@
 extends NinePatchRect
 
+const SPEED = 5
+
+signal finishedLerp
+
+@onready var anchorDiff = anchor_bottom - anchor_top
+var shown_anchor: float = 1
+var hidden_anchor: float = 1.5
+var target_anchor: float = hidden_anchor #THIS IS FOR TOP ANCHOR
 var queue = [];
 
+func _show() -> int:
+	target_anchor = shown_anchor
+	await finishedLerp
+	return 1
+
+func _hide():
+	target_anchor = hidden_anchor
+	
 
 func display(text: String = "....", displayName: String = "?????", icon: Resource = load("res://graphics/dialogSystem/mysteriousIcon.png"), typingSpeed: float = 0.15) -> int:
-	
+	_show()	
 	%Dialog.text = ""
 	
-	%DisplayName.text = displayName
+	%DisplayName.text = "[center]%s[/center]" % displayName
 	
 	%Icon.texture = icon
 	
@@ -15,12 +31,19 @@ func display(text: String = "....", displayName: String = "?????", icon: Resourc
 	
 	for character in splittedText:
 		%Dialog.text+=character
-		await get_tree().create_timer(typingSpeed).timeout
+		await get_tree().create_timer(typingSpeed/2).timeout
 		AudioManager.play(AudioManager.SoundIds.DIALOG)
 	
 	return 0
 
 
 func _ready() -> void:
+	_hide()
 	await get_tree().create_timer(3).timeout
-	display()
+	display("HEJKFL:LJSLFKLDJFSKLDFJLSJDFLKSJKLFSJKLFDJSLDFJLKJFKLJDFKLj")
+	
+func _process(delta: float) -> void:
+	anchor_top = lerp(anchor_top, target_anchor, delta*SPEED)
+	if anchor_top == target_anchor:
+		finishedLerp.emit()
+	anchor_bottom = lerp(anchor_bottom, target_anchor+anchorDiff, delta*SPEED)
