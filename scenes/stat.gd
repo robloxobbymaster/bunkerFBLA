388033d35a93bdfea5155a652b2a85874dad4256
robloxobbymaster <kevinxliu2010@gmail.com>
2025-01-28 19:56:00 -0600

    Initial commit

---
+++ scenes/stat.gd
@@ -0,0 +1,19 @@
+extends TextureProgressBar
+class_name statParent
+
+@export var degration: float:
+	set(value):
+		degration = value
+
+@export var val: int:
+	set(value):
+		val = value
+		var tween = get_tree().create_tween()
+		tween.tween_property($".", "value", value, 1)
+
+func _process(_delta: float) -> void:
+	$".".value = val
+	
+
+func _on_degration_timer_timeout() -> void:
+	val -= degration
