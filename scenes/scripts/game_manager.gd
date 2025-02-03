e3d83063f190684247b0cf027ede886d75dd85df
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2025-02-03 09:24:19 -0600

    Update game_manager.gd

--- scenes/scripts/game_manager.gd
+++ scenes/scripts/game_manager.gd
@@ -1,17 +1,17 @@
 extends Node
 
 @export var SCORE: int = 0
-@export var HEALTH: int = 100:
+@export var HEALTH: int = GUI.HEALTH.val:
 	set(value):
 		HEALTH = value
 		GUI.HEALTH.val = value
 		
-@export var HUNGER: int = 100:
+@export var HUNGER: int = GUI.HUNGER.val:
 	set(value):
 		HUNGER = value
 		GUI.HUNGER.val = value
 		
-@export var THIRST: int = 100:
+@export var THIRST: int = GUI.THIRST.val:
 	set(value):
 		THIRST = value
 		GUI.THIRST.val = value
