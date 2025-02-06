2d3ebba9f31566014252966543594fbeede082c3
Jaiden Khosla <jaiden@khosla.io>
2025-02-05 20:58:20 -0600

    more work

--- scenes/scripts/reciever.gd
+++ scenes/scripts/reciever.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
 @export var connector: Wire
+@export var is_connected: bool = false
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -12,3 +13,4 @@ func _on_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_inde
 	if wire == connector:
 		wire.move_endpoint(get_node("Area2D").global_position)
 		wire.can_move = false
+		is_connected = true
